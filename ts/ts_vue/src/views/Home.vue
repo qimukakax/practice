f83b1f5862c6d5a4d81e994443e0872eb72efbb6
qimukakax <1170848082@qq.com>
2020-03-10 17:36:25 +0800

    ts  与  list练习

---
+++ ts/ts_vue/src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="home">
+  </div>
+</template>
+
+<script lang="ts">
+// @ is an alias to /src
+export default {
+  components: {
+  }
+}
+</script>
+<style lang="sass" scoped>
+
+</style>
